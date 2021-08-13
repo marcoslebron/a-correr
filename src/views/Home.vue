cbf084b72be40518a818ee6d7c1bb68891a1cc3a
Marcos lebron <marcos@commonshare.com>
2021-08-13 19:03:04 -0400

    chore: update views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,8 @@ main
     .col-md-9
       .form-group
         input.form-control(type="text" v-model="query" placeholder="Buscar la mejor imagen")
+    .col-md-2
+      button(@click="searchImages") Search
   .row.justify-content-md-center
     .col-md-6
       SellerProgressBar(v-for="sellerCompetitor in sellersPoints" 
@@ -14,13 +16,13 @@ main
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import SellerImage from "@/components/SellerImage";
 import SellerProgressBar from "@/components/SellerProgressBar";
 import { axiosUnplash } from "@/vue-http";
 import sellersJson from "@/sellers.json";
 import imagesJson from "@/images.json";
-import { State } from "vuex-class";
+import { State, Getter } from "vuex-class";
 
 @Component({
   components: {
@@ -30,12 +32,18 @@ import { State } from "vuex-class";
 })
 export default class Home extends Vue {
   @State sellersPoints;
+  @Getter raceHasEnded;
+
   query = "";
+
   images = imagesJson.results.map(item => item.urls);
+
   sellers = sellersJson;
+
   // mounted(): void {
   //   this.fetchImages();
   // }
+
   mounted(): void {
     this.assingImageToSeller()
   }
@@ -49,9 +57,19 @@ export default class Home extends Vue {
   assingImageToSeller(): void {
     this.sellers.forEach((seller, index) => {
       let randomImage = this.images[index]
-      console.log(randomImage)
       this.$set(seller,'image',randomImage)
     })
+  }
+
+  dispatchAlegraInvoice(): void {
+    console.log("dispaching invoice")
+  }
+
+  @Watch("raceHasEnded", {immediate: true})
+  onRaceEnded(val: boolean): void{
+    if (val) {
+      this.dispatchAlegraInvoice()
+    }
   }
 }
 </script>
---
+++ src/views/Invoice.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="invoice">
+    <h1>This is n invoice page</h1>
+  </div>
+</template>
