5637619c36f95ba4e7120cba9b580d9b23f1b33b
Marcos lebron <marcos@commonshare.com>
2021-08-18 18:18:51 -0400

    chore: test

--- tests/unit/SellerProgressBar.spec.ts
+++ tests/unit/SellerProgressBar.spec.ts
@@ -23,4 +23,27 @@ describe("SellerProgressBar.vue", () => {
   it("renders props.seller when passed", () => {
     expect(wrapper.text()).toMatch(name);
   });
+
+  it("shows seller remaining point to win", async () => {
+    wrapper.setProps({ sellerPoints: 6 });
+
+    const expectedRemainingPoints = wrapper.vm.$store.state.winnerPoints - 6;
+
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find(".seller-progress-points-win").text()).toBe(
+      `Puntos para Ganar ${expectedRemainingPoints}`
+    );
+  });
+
+  it("shows winner seller text", async () => {
+    wrapper.setProps({ sellerPoints: 20 });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find(".seller-progress-winner").text()).toMatch(
+      "¡¡Ganador!!"
+    );
+  });
+
+  it("shows seller points", () => {
+    expect(wrapper.text()).toMatch(`${wrapper.vm.$props.sellerPoints} Puntos`);
+  });
 });
